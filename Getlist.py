7564c04a6286e938bdd231490d8c9a1ff96aea99
Francesco P <60569057+FrancescoLFM@users.noreply.github.com>
2021-02-18 20:47:26 +0100

    Delete Getlist.py

--- Getlist.py
+++
@@ -1,5 +0,0 @@
-def getlist(update, context):
-    with open("File.json", 'rb') as file:
-        context.bot.send_document(chat_id=update.message.chat_id,
-                                  document=file, filename="File.json",
-                                  caption="Ecco la lista più recente salvata")
