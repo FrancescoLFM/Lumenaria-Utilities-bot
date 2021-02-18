478f4195e1bd088eb96072b68376e73ddd5cfd8a
Francesco P <60569057+FrancescoLFM@users.noreply.github.com>
2021-02-18 20:39:22 +0100

    Add files via upload

---
+++ Load.py
@@ -0,0 +1,25 @@
+import json
+from Variables import c, pvt_text
+from telegram import ParseMode
+
+
+def load(update, context):
+    try:
+        if update.message.chat.id == c:
+            with open("File.json") as file:
+                a = json.load(file)
+            for i in range(1, len(a)+1):
+                a[i-1] = '%s) ' % i + a[i-1]
+            context.bot.send_message(chat_id=c,
+                                     text="<b>Lista degli attuali cittadini</b>:\n"+('\n'.join(a)),
+                                     parse_mode=ParseMode.HTML)
+        else:
+            context.bot.send_message(chat_id=update.message.chat.id,
+                                     text=pvt_text,
+                                     parse_mode=ParseMode.HTML)
+    except UnicodeDecodeError:
+        context.bot.send_message(chat_id=c, text="Errore fatale! Assicurati che il file sia in formato JSON")
+    except TypeError:
+        context.bot.send_message(chat_id=c,
+                                 text="""Non inserire numeri nel file!\n
+                                      (Se vuoi inserirli devi mettere le virgolette)""")
---
+++ Variables.py
@@ -0,0 +1,3 @@
+pvt_text = "Attenzione! Funziono solo nella piazza di <a href='t.me/RepubblicaLumenaria'>Lumenaria</a>"
+c = -466724082
+TOKEN = "1535282631:AAFX7bS6oYsCHMAIFs5itBNvyDrDdkQflHU"
