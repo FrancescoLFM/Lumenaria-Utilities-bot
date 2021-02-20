f3d88ed83a9a7beb5890c19d086a2c543d6f7e3f
Giovanni Zaccaria <71092108+LoZack19@users.noreply.github.com>
2021-02-20 09:55:23 +0100

    Delete Genlist.py

--- commands/Genlist.py
+++
@@ -1,5 +0,0 @@
-def genfile(update, context):
-  with  file as open("File.json", "w"):
-    file.write("[]")
-  context.bot.send_message(chat_id=c,
-                           text="Nuova lista vuota generata.")
