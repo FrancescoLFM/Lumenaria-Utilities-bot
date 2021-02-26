1458a7d64f73b6c41a1e93e860b72724e3d013a3
Francesco P <60569057+FrancescoLFM@users.noreply.github.com>
2021-02-26 16:59:59 +0100

    Added error handler
    Correct void list exception

--- commands/Add.py
+++ commands/Add.py
@@ -1,17 +1,25 @@
 from telegram import ParseMode
 import json
 from commands.Variables import c, pvt_text
+from commands.Newlist import genlist
 import html
 
 
-def add(update, context):
+def add_print(update, context, filename):
+    user = update.message.from_user
+    add(update, context, filename)
+    context.bot.send_message(chat_id=c, text="""Cittadino aggiunto con successo!\n\nRichiesto da: @"""
+                                             + user['username'])
+
+
+def add(update, context, filename):
+    exe = False
     try:
-        if update.message.chat.id == c:
-            user = update.message.from_user
-            txt = update.message.text.split(" ", 1)
+        if update.effective_chat.id == c:
+            txt = update.message.text.split(" ")
             txt.remove("/newcit")
-            
-            if len(txt) == 3:
+            print(len(txt))
+            if len(txt) <= 3:
                 for i in range(len(txt)):
                     if len(txt[i]) <= 30:
                         exe = True
@@ -19,21 +27,16 @@ def add(update, context):
                         raise ValueError
             else:
                 raise ValueError
-            
-            # Issue from LoZack solved
-            
             if exe:
                 temp = " ".join(txt)
                 print(txt)
-                with open("File.json", "r+") as file:
+                with open(filename, "r+") as file:
                     a = json.load(file)
                     a.append(html.escape(temp))
                     file.close()
-                with open("File.json", "w") as file:
+                with open(filename, "w") as file:
                     json.dump(a, file)
                     file.close()
-                context.bot.send_message(chat_id=c, text="""Cittadino aggiunto con successo!\n\nRichiesto da: @"""
-                                                         + user['username'])
         else:
             context.bot.send_message(chat_id=update.message.chat.id,
                                      text=pvt_text,
@@ -42,4 +45,13 @@ def add(update, context):
         context.bot.send_message(chat_id=c, text="""Sintassi erata, inserire parametri dopo /newcit
                                                  \nEsempio: /newcit Giorgio Stramaroni @GiorgioStramaroni""")
     except ValueError:
-        context.bot.send_message(chat_id=c, text="""Nome troppo lungo! Assicurati di aver inserito massimo 30 caratteri""")
+        try:
+            temp = "temp.json"
+            genlist(temp)
+            add(update, context, temp)
+            file = "File.json"
+            genlist(file)
+            add_print(update, context, file)
+        except ValueError:
+            context.bot.send_message(chat_id=c, text="""Nome troppo lungo! Assicurati di aver
+                                                        inserito massimo 30 caratteri""")
