d75b601e3cf1ecf0d82fb8d2a847448d9c639b25
Francesco P <60569057+FrancescoLFM@users.noreply.github.com>
2021-02-26 17:02:10 +0100

    Fixed

--- bot.py
+++ bot.py
@@ -1,20 +1,27 @@
 # Code by FrancescoLFM 2021
 # No licences, free to use this code
 import commands
-from commands import TOKEN
-from telegram.ext import CommandHandler
+from commands import TOKEN, filename
+from telegram.ext import CommandHandler, Updater, CallbackContext
+import telegram
+import functools
 
-while 1:
+
+def main():
     updater = Updater(token=TOKEN, use_context=True)
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler('start', Start.start))
-    dispatcher.add_handler(CommandHandler('newlist', Newlist.newlist))
-    dispatcher.add_handler(CommandHandler('cittadini', Load.load))
-    dispatcher.add_handler(CommandHandler('newcit', Add.add))
-    dispatcher.add_handler(CommandHandler('delcit', Del.delete))
-    dispatcher.add_handler(CommandHandler('getlista', Getlist.getlist))
-
+    dispatcher.add_handler(CommandHandler('start', commands.Start.start))
+    dispatcher.add_handler(CommandHandler('newlist', commands.Newlist.newlist))
+    dispatcher.add_handler(CommandHandler('cittadini', commands.Load.load))
+    dispatcher.add_handler(CommandHandler('newcit', commands.Add.add(update=telegram.Update, context=CallbackContext,
+                                                                     filename=filename)))
+    dispatcher.add_handler(CommandHandler('delcit', commands.Del.delete))
+    dispatcher.add_handler(CommandHandler('getlista', commands.Getlist.getlist))
 
     updater.start_polling()
     updater.idle()
-    continue
+
+
+if __name__ == '__main__':
+    main()
+
